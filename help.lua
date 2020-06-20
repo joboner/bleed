9949477c1803b10f2d24c58198d948410ecda976
jon <67181870+joboner@users.noreply.github.com>
2020-06-19 23:13:54 -0700

    Create help.lua

---
+++ help.lua
@@ -0,0 +1,5 @@
+you run into any bugs or have questions: contact me @ jon#0006
+note: you can use 'help <command>* <subcommand>' for further information on how to use a command or a list of subcommands for a command like lastfm
+
+default prefix: *, you change it by running 'prefix set <prefixhere>'
+join discord server for better support or whatever: https://bleed.win/discord or https://discord.gg/T3epc7R
